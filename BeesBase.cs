4d746edfab24b9f78e8c3506daa3a1ce1ab85338
Dkspawner1 <NicholasChrysanthou123@gmail.com>
2023-01-27 16:59:23 -0500

    Generic Bees Added

--- BeesBase.cs
+++ BeesBase.cs
@@ -2,19 +2,26 @@ namespace BSSCalculator;
 
 public partial class BeesBase
 {
+    public KeyValuePair<int, string>? BeeEnum { get; protected set; }
+    [Flags]
+    public enum BeeColour : byte { ColourLess, Red, Blue }
     public double BaseEnergy { get; protected set; }
     public double BaseSpeed { get; protected set; }
     public double BaseAttack { get; protected set; }
     public double BaseGather { get; protected set; }
     public double BaseProduction { get; protected set; }
-    public BeesBase(double baseEnergy, double baseSpeed, double baseAttack, double baseGather, double baseProduction)
+
+    public BeeColour Colour { get; protected set; }
+    public BeesBase(KeyValuePair<int, string>? beeEnum, BeeColour colour, double baseEnergy, double baseSpeed, double baseAttack, double baseGather, double baseProduction)
     {
+        BeeEnum = beeEnum;
+        Colour = colour;
         BaseEnergy = baseEnergy;
         BaseSpeed = baseSpeed;
         BaseAttack = baseAttack;
         BaseGather = baseGather;
         BaseProduction = baseProduction;
     }
-
+    public void ListBee() => Console.WriteLine($"{BeeEnum.Value} {Colour} {BaseEnergy} {BaseSpeed} {BaseAttack} {BaseGather} {BaseProduction}");
 
 }
--- Globals.cs
+++ Globals.cs
@@ -2,10 +2,11 @@ global using global::System;
 global using global::System.Numerics;
 global using static global::System.Math;
 global using global::System.Globalization;
-global using global::System.Text.Json;  
+global using global::System.Text.Json;
 global using global::System.Collections;
 global using global::System.Collections.Generic;
 global using global::System.Runtime.InteropServices;
 global using global::System.Linq;
+global using global::System.IO;
 namespace BSSCalculator;
 
